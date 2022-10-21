a6e5e7adf5f9694c6b0245e3fd3820991e89c727
AlexanderB0ndarenko <alexanderbondarenko@coherentsolutions.com>
2022-10-21 17:02:32 +0300

    initial changes

---
+++ Tasks/NET02_1/NET02_1/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NET02_1
+{
+    class Book
+    {
+        [Required]
+        [StringLength(1, MinimumLength = 1000)]
+        public string Title { get; private set; }
+
+        [RegularExpression(@"\d{3}-?\d-?\d{2}-?\d{6}-?\d{1}")]
+        public string ISBN { get; private set; }
+
+        public DateTime Date;
+
+        List<Author> authors;
+
+        public Book(string title)
+        {
+            Title = title;
+        }
+    }
+}
---
+++ Tasks/NET02_1/NET02_1/Catalog.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET02_1
+{
+    class Catalog
+    {
+    }
+}
