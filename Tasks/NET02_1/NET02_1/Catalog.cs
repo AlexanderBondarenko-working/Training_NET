1c8c39d41e2e151458a89930154188d2f2ad4bec
AlexanderB0ndarenko <alexanderbondarenko@coherentsolutions.com>
2022-10-25 16:23:48 +0300

    catalog class

--- Tasks/NET02_1/NET02_1/Catalog.cs
+++ Tasks/NET02_1/NET02_1/Catalog.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace NET02_1
 {
-    class Catalog
+    class Catalog : KeyedCollection<string, Book>
     {
+        public Catalog() : base(new ISBNEqualityComparer()) { }
+        protected override string GetKeyForItem(Book item)
+        {
+            return item.ISBN;
+        }
+    }
+
+    class ISBNEqualityComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string ISBN1, string ISBN2)
+        {
+            return Regex.Replace(ISBN1, "-", "").Equals(Regex.Replace(ISBN2, "-", ""));
+        }
+
+        public int GetHashCode(string ISBN)
+        {
+            return Regex.Replace(ISBN, "-", "").GetHashCode();
+        }
     }
 }
