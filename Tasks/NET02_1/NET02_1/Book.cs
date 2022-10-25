15746a94eabb2c93402aa3f38c91785ed8072a93
AlexanderB0ndarenko <alexanderbondarenko@coherentsolutions.com>
2022-10-25 14:52:48 +0300

    book data validation and equals mrthod

--- Tasks/NET02_1/NET02_1/Book.cs
+++ Tasks/NET02_1/NET02_1/Book.cs
@@ -2,25 +2,48 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace NET02_1
 {
     class Book
     {
-        [Required]
-        [StringLength(1, MinimumLength = 1000)]
+        List<Author> _authors = new List<Author>();
+
+        public const string PatternISBN = @"\d{13}";
+        public const string DashPatternISBN = @"\d{3}-\d-\d{2}-\d{6}-\d{1}";
+        public const string LengthPatternTitle = @"\w{1,1000}";
+
         public string Title { get; private set; }
-
-        [RegularExpression(@"\d{3}-?\d-?\d{2}-?\d{6}-?\d{1}")]
         public string ISBN { get; private set; }
+        public DateTime Date { get; set; } = default;
 
-        public DateTime Date;
-
-        List<Author> authors;
-
-        public Book(string title)
+        public Book(string title, string ISBN)
         {
+            if(!Regex.IsMatch(title, LengthPatternTitle))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if(!Regex.IsMatch(ISBN, PatternISBN) && !Regex.IsMatch(ISBN, DashPatternISBN))
+            {
+                throw new FormatException();
+            }
+            this.ISBN = ISBN;
             Title = title;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Book book)
+            {
+                return Regex.Replace(this.ISBN, "-", "").Equals(Regex.Replace(book.ISBN, "-", ""));
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 }
