7c11cc4f5abf941ad3ffaad549e59dae6601a6fb
AlexanderBondarenko <alexanderbondarenko@coherentsolutions.com>
2022-11-01 16:21:45 +0300

    logins output

---
+++ Tasks/Task02_2/Task02_2/ConfigurationHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Task02_2
+{
+    public static class ConfigurationHelper
+    {
+        public static readonly string LoginElementName = "login";
+        public static readonly string LoginNameAttribute = "name";
+        public static readonly string WindowElementName = "window";
+        public static readonly string WindowTitleAttribute = "title";
+        public static readonly string TopElementName = "top";
+        public static readonly string LeftElementName = "left";
+        public static readonly string WidthElementName = "width";
+        public static readonly string HeightElementName = "height";
+
+        public static string LoginsToString(this IEnumerable<XElement> logins)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var login in logins)
+            {
+                sb.AppendLine($"{login.Name}: {login.Attribute(LoginNameAttribute).Value}");
+                var windws = login.Elements(WindowElementName);
+                if (windws.Any())
+                {
+                    foreach (var window in windws)
+                    {
+                        sb.AppendLine($"    {window.Attribute(WindowTitleAttribute).Value}(" +
+                            $"{window.Element(TopElementName)?.Value ?? "?"}," +
+                            $"{window.Element(LeftElementName)?.Value ?? "?"}," +
+                            $"{window.Element(WidthElementName)?.Value ?? "?"}," +
+                            $"{window.Element(HeightElementName)?.Value ?? "?"})");
+                    }
+                }
+                else
+                {
+                    sb.AppendLine(login.Value);
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static bool IsLoginCorrect(this XElement login)
+        {
+            var mainElements = login.Elements(WindowElementName).Where(element => element.Attribute(WindowTitleAttribute).Value == "main");
+            if (mainElements.Count() <= 1)
+            {
+                var mainElement = mainElements.FirstOrDefault();
+                return mainElement == null ? true : 
+                    mainElement.Element(TopElementName) != null &&
+                    mainElement.Element(LeftElementName) != null &&
+                    mainElement.Element(WidthElementName) != null &&
+                    mainElement.Element(HeightElementName) != null;
+            }
+            return false;
+        }
+
+        public static IEnumerable<XElement> Logins(this XDocument document)
+        {
+            return document.Root.Elements(LoginElementName);
+        }
+    }
+}
--- Tasks/Task02_2/Task02_2/Program.cs
+++ Tasks/Task02_2/Task02_2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace Task02_2
 {
@@ -6,6 +8,11 @@ namespace Task02_2
     {
         static void Main(string[] args)
         {
+            XDocument document = XDocument.Load(@"..\..\..\Config\Data.xml");
+
+            Console.Write(document.Logins().LoginsToString());
+
+            Console.Write(document.Logins().Where(login => login.IsLoginCorrect()).LoginsToString());
         }
     }
 }
